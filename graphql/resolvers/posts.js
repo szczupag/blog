6880ce5a86fbfab1102079eb0daccd9605f753ef
Agata Szczuka <agata.szczuka@gmail.com>
2021-01-17 02:11:52 +0100

    Create mutations to create and delete posts.

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,11 +1,57 @@
+const { AuthenticationError } = require('apollo-server');
 const Post = require('../../models/Post');
+const checkAuth = require('../../utils/checkAuth');
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error('Post not found');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  },
+  Mutation: {
+    async createPost(_, { title, body }, context) {
+      const user = checkAuth(context);
+
+      const newPost = new Post({
+        title,
+        body,
+        user: user.id,
+        author: user.username,
+        createdAt: new Date().toISOString()
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.author) {
+          await post.delete();
+          return 'Post deleted successfully';
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
       } catch (err) {
         throw new Error(err);
       }
